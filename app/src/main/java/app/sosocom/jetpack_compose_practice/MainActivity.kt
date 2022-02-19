2bc196bca637efc0fbceba6f59db998769bc8e36
KangSeungho <sosocom95@naver.com>
2022-02-19 12:37:43 +0900

    - 1. Column, Row, Text 컴포넌트 활용

--- app/src/main/java/app/sosocom/jetpack_compose_practice/MainActivity.kt
+++ app/src/main/java/app/sosocom/jetpack_compose_practice/MainActivity.kt
@@ -3,41 +3,48 @@ package app.sosocom.jetpack_compose_practice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import app.sosocom.jetpack_compose_practice.ui.theme.Jetpack_compose_practiceTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Jetpack_compose_practiceTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+        setContent { SetView() }
+    }
+}
+
+@Composable
+fun SetView() {
+    Jetpack_compose_practiceTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = Color.Blue)
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Hello")
+                Spacer(Modifier.width(16.dp))
+                Text("World")
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Jetpack_compose_practiceTheme {
-        Greeting("Android")
-    }
+fun SetViewPreview() {
+    SetView()
 }
