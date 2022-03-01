8399c30abce7bba9def6ab54a5f1faa18539df24
KangSeungho <sosocom95@naver.com>
2022-03-01 19:58:50 +0900

    [NT-7] AddEditNoteScreen 생성

--- app/src/main/java/app/sosocom/jetpack_compose_practice/feature_note/presentation/add_edit_note/AddEditNoteEvent.kt
+++ app/src/main/java/app/sosocom/jetpack_compose_practice/feature_note/presentation/add_edit_note/AddEditNoteEvent.kt
@@ -7,6 +7,6 @@ sealed class AddEditNoteEvent {
     data class ChangeTitleFocus(val focusState: FocusState): AddEditNoteEvent()
     data class EnteredContent(val value: String): AddEditNoteEvent()
     data class ChangeContentFocus(val focusState: FocusState): AddEditNoteEvent()
-    data class ChangeColorFocus(val color: Int): AddEditNoteEvent()
+    data class ChangeColor(val color: Int): AddEditNoteEvent()
     object SaveNote: AddEditNoteEvent()
 }
