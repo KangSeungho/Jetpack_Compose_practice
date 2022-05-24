f1682dd768bd5daeb728f1c928a4c22b8c5026a7
KangSeungho <sosocom95@naver.com>
2022-05-24 14:43:49 +0900

    [NT-10] Gradle 버전 다운그레이드 및 라이브러리 업데이트

--- app/src/main/java/app/sosocom/jetpack_compose_practice/di/AppModule.kt
+++ app/src/main/java/app/sosocom/jetpack_compose_practice/di/AppModule.kt
@@ -1,6 +1,6 @@
 package app.sosocom.jetpack_compose_practice.di
 
-import android.app.Application
+import android.content.Context
 import androidx.room.Room
 import app.sosocom.jetpack_compose_practice.feature_note.data.data_source.NoteDatabase
 import app.sosocom.jetpack_compose_practice.feature_note.data.repository.NoteRepositoryImpl
@@ -9,6 +9,7 @@ import app.sosocom.jetpack_compose_practice.feature_note.domain.use_cases.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -18,22 +19,28 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideNoteDatabase(app: Application) = Room.databaseBuilder(
-        app,
-        NoteDatabase::class.java,
-        NoteDatabase.DATABASE_NAME
-    ).build()
+    fun provideNoteDatabase(@ApplicationContext context: Context): NoteDatabase {
+        return Room.databaseBuilder(
+            context,
+            NoteDatabase::class.java,
+            NoteDatabase.DATABASE_NAME
+        ).build()
+    }
 
     @Provides
     @Singleton
-    fun provideNoteRepository(db: NoteDatabase) = NoteRepositoryImpl(db.noteDao)
+    fun provideNoteRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
 
     @Provides
     @Singleton
-    fun provideNoteUseCases(repository: NoteRepository) = NoteUseCases(
-        getNotes = GetNotes(repository),
-        deleteNote = DeleteNote(repository),
-        addNote = AddNote(repository),
-        getNote = GetNote(repository)
-    )
+    fun provideNoteUseCases(repository: NoteRepository): NoteUseCases {
+        return NoteUseCases(
+            getNotes = GetNotes(repository),
+            deleteNote = DeleteNote(repository),
+            addNote = AddNote(repository),
+            getNote = GetNote(repository)
+        )
+    }
 }
